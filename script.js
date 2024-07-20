badf11411cd64db50f8cb97fd0313d2bc9084c91
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2024-07-20 22:34:15 +1000

    formatting

--- script.js
+++ script.js
@@ -30,9 +30,14 @@ function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-                          { style: "currency", currency: "USD",
-                            minimumFractionDigits: 2 }).format;
+
+    const format = new Intl.NumberFormat(
+        "en-US",
+        { style: "currency", 
+          currency: "USD",
+          minimumFractionDigits: 2 
+        }).format;
+
     for (let perf of invoice.performances) {
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
@@ -43,8 +48,10 @@ function statement (invoice, plays) {
       result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
     }
+
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
+    
     return result;
   }
   
