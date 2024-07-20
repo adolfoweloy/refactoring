44538c138804fe17c361dc3cc19edf7900c07f56
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2024-07-20 22:34:16 +1000

    move volume credits in the same way total amount was moved

--- createStatementData.js
+++ createStatementData.js
@@ -25,6 +25,15 @@ class PerformanceCalculator {
         }  
         return result;
     }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type) {
+            result += Math.floor(this.performance.audience / 5);
+        }
+        return result;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -37,25 +46,15 @@ export default function createStatementData(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
-
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) {
-            result += Math.floor(aPerformance.audience / 5);
-        }
-        return result;
     }
 
     function totalVolumeCredits(data) {
