ceca5be143d21d5fcac89084f2b1ca46f9865387
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2024-07-20 22:34:15 +1000

    extract method: extracts the logic to get volumeCredits

--- script.js
+++ script.js
@@ -1,51 +1,6 @@
 import { plays, invoices } from './data.js'
 
-function playFor(aPerformance) {
-    // in the book, the author is probably relying on plays as something available in the scope of
-    // the function. I personally find that a bit messy, regardless if the function is declared
-    // within or witout statement()
-    return plays[aPerformance.playID];
-}
 
-function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-          result = 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-        case "comedy":
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default:
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }  
-    return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) {
-        result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
-}
-
-function usd(aNumber) {
-    return new Intl.NumberFormat(
-        "en-US",
-        { style: "currency", 
-          currency: "USD",
-          minimumFractionDigits: 2 
-        }).format(aNumber);
-}
 
 function statement (invoice, plays) {
     let totalAmount = 0;
@@ -56,15 +11,63 @@ function statement (invoice, plays) {
       totalAmount += amountFor(perf);
     }
 
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
+    let volumeCredits = totalVolumeCredits();
 
     result += `Amount owed is ${usd(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
 
     return result;
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+    
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+              result = 40000;
+              if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
+              }
+              break;
+            case "comedy":
+              result = 30000;
+              if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+              }
+              result += 300 * aPerformance.audience;
+              break;
+            default:
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        }  
+        return result;
+    }
+    
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type) {
+            result += Math.floor(aPerformance.audience / 5);
+        }
+        return result;
+    }
+    
+    function usd(aNumber) {
+        return new Intl.NumberFormat(
+            "en-US",
+            { style: "currency", 
+              currency: "USD",
+              minimumFractionDigits: 2 
+            }).format(aNumber);
+    }
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+    }
   }
   
 console.log(statement(invoices[0], plays))
