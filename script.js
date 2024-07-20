514f084cda0a09c5996f0d18bee194fedba170c8
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2024-07-20 22:34:15 +1000

    slide statements: move volumeCredits close to the loop
    By moving the variable close to the loop that actually uses it, makes
    things easier to apply extract method.

--- script.js
+++ script.js
@@ -49,7 +49,6 @@ function usd(aNumber) {
 
 function statement (invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
@@ -57,6 +56,7 @@ function statement (invoice, plays) {
       totalAmount += amountFor(perf);
     }
 
+    let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
